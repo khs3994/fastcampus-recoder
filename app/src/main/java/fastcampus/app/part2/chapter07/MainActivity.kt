4e140d02f11f315d62249e32cb843c0cff3ba259
김현승 <80810303+khs3994@users.noreply.github.com>
2022-07-11 20:13:56 +0900

    :sparkles: :: 권한 기능 추가

--- app/src/main/java/fastcampus/app/part2/chapter07/MainActivity.kt
+++ app/src/main/java/fastcampus/app/part2/chapter07/MainActivity.kt
@@ -1,5 +1,7 @@
 package fastcampus.app.part2.chapter07
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,16 +10,40 @@ class MainActivity : AppCompatActivity() {
     private val recordButton: RecordButton by lazy {
         findViewById(R.id.record_btn)
     }
-
+    private val audioPermission = arrayOf(Manifest.permission.RECORD_AUDIO)
     private var state = State.BEFORE_RECORDING
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestPermission()
         initViews()
+    }
+
+    // 권한이 허용되었는지 여기서 확인
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        val audioRecordPermissionGranted =
+            requestCode == REQUEST_RECORD_AUDIO_PERMISSION && grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED
+
+        if(!audioRecordPermissionGranted){ //권한이 부여가 되지 않았을 경우
+            finish()// 앱 종료
+        }
+    }
+
+    private fun requestPermission() {
+        requestPermissions(audioPermission, REQUEST_RECORD_AUDIO_PERMISSION)
     }
 
     private fun initViews() {
         recordButton.updateIcon(state)
+    }
+
+    companion object {
+        private const val REQUEST_RECORD_AUDIO_PERMISSION = 201
     }
 }
